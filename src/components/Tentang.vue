acaef4355590069306888261d9135964f7dc78c3
yayanheeh <tokoxp58@gmail.com>
2019-07-31 20:11:05 +0700

    Create Tentang.vue

---
+++ src/components/Tentang.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <v-card flat>
+    <v-card-title class="grey lighten-5">
+      Prosesnya semi-otomatis.
+      <br/> 1. Tambahkan nomor Anda untuk mengirim tautan
+      <br/> 2. Tambahkan nomor untuk dikirim
+      <br/> 3. Tulis pesan siaran
+      <br/> 4. Klik kirim
+      <br/> <span class="red--text">5. Anda harus 
+   menerima pesan dengan LINK daftar siaran (Tidak tersedia)</span>
+    </v-card-title> 
+</v-card>
+</template>
