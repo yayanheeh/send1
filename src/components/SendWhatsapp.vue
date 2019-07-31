87ed0fbb23cadb1317d99d05d1c377227c537340
yayanheeh <tokoxp58@gmail.com>
2019-07-31 18:11:45 +0700

    Update SendWhatsapp.vue

--- src/components/SendWhatsapp.vue
+++ src/components/SendWhatsapp.vue
@@ -1,24 +1,24 @@
 <template>
-  <form v-on:submit.prevent="sendWhatsapp">
+  <form v-on:submit.prevent="send Whatsapp">
     <v-card flat>
       <v-card-text>
-        <v-autocomplete autocomplete :items="code" item-value="callingCode" v-model="country.default" label="País"
-          item-text="name" :error-messages="errors.collect('country')" v-validate="'required'" data-vv-name="country"
+        <v-autocomplete autocomplete :items="code" item-value="callingCode" v-model="country.default" label="negara"
+          item-text="name" :error-messages="errors.collect('country')" v-validate="'required'" data-vv-name="negara"
           required return-object></v-autocomplete>
       </v-card-text>
       <v-card-text>
-        <v-text-field type="number" v-model="phoneNumber" auto-focus placeholder="987654321" label="Número"
-          :error-messages="errors.collect('phoneNumber')" v-validate="'required|numeric'" data-vv-name="phoneNumber"
+        <v-text-field type="number" v-model="phoneNumber" auto-focus placeholder="987654321" label="Nomor"
+          :error-messages="errors.collect('phoneNumber')" v-validate="'required|numeric'" data-vv-name="nomor telepon"
           required></v-text-field>
       </v-card-text>
       <v-card-text>
-        <v-textarea v-model="message.text" :label="label" placeholder="Hello ..." auto-grow></v-textarea>
+        <v-textarea v-model="message.text" :label="label" placeholder="Pesan text..." auto-grow></v-textarea>
       </v-card-text>
       <v-card-actions>
         <v-layout wrap row text-xs-center>
           <v-flex xs6 offset-xs3>
             <v-btn block class="primary" type="submit" dark center>
-              <v-icon class="mr-2" dark>fab fa-whatsapp</v-icon>Enviar Mensaje
+              <v-icon class="mr-2" dark>fab fa-whatsapp</v-icon>Kirim Pesan
             </v-btn>
           </v-flex>
         </v-layout>
